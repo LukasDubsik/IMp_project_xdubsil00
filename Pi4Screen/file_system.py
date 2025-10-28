62dc646e49f4a8ef2781fd5d6d3dbce44b6caace
LukasDubsik <luke@bubaci.net>
2025-10-28 12:00:36 +0100

    Commenetd the Pi side of code

--- Pi4Screen/file_system.py
+++ Pi4Screen/file_system.py
@@ -1,6 +1,13 @@
+#Imports
 import serial
+
+#Set the serial communication - baud agreed upon beforehand
 ser = serial.Serial('/dev/serial0', 115200, timeout=1)
-print("Listening on /dev/serial0 @115200 ...")
+
+#Start the program and print the values
+print("Starting the Console for the IMP project...")
+
+#Infinitely listen to the D1R32 processing outputs
 while True:
     line = ser.readline().decode(errors='ignore').strip()
     if line:
