281a3522cb48c54ebc90866ef3ff68dd954a4469
LukasDubsik <luke@bubaci.net>
2025-10-29 14:35:28 +0100

    Mounting of the filesystem done

--- D1R32/main/periferies/little_fs.c
+++ D1R32/main/periferies/little_fs.c
@@ -1,7 +1,14 @@
 //User Includes
 #include "little_fs.h"
+#include "led.h"
+#include "../imp.h"
 
+//Systemn Includes
 #include "esp_littlefs.h"
+#include "esp_log.h"
+
+//Standart Includes
+#include <sys/unistd.h>
 
 
 void mount_little_fs(void)
@@ -16,9 +23,29 @@ void mount_little_fs(void)
 
     //Register and immediately mount a bare filesystem at the specified partition
     esp_err_t error = esp_vfs_littlefs_register(&conf);
-
     //If the mounting failed
     if (error != ESP_OK){
+        //Blink the error code
+        blink_error(LED_MOUNTING_FAILED);
+        //Trigger full system reboot
+        abort();
+    }
+
+    //Get information about current system configuration
+    size_t total = 0, used = 0;
+    error = esp_littlefs_info(conf.partition_label, &total, &used);
+    //Check that the system is stable
+    if (error == ESP_OK){
+        //Log for debugging
+        ESP_LOGI(TAG, "LittleFS mounted at %s (%u KB total, %u KB used)", conf.base_path, total, used);
+        //Later will be used when getting system data
+    }
+
+    //Start at this directory when starting system
+    if (chdir("/littlefs") != 0) {
+        //Blink the error code
+        blink_error(LED_MOUNTING_FAILED);
+        //Trigger full system reboot
         abort();
     }
 }
