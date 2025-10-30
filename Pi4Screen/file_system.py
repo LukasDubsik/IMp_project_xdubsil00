8b0dfd30bc74468f3627680d1874b54698bf07f5
LukasDubsik <luke@bubaci.net>
2025-10-30 09:31:16 +0100

    Reader on the Python side for user commands implementation

--- Pi4Screen/file_system.py
+++ Pi4Screen/file_system.py
@@ -1,5 +1,17 @@
 #Imports
 import serial
+import sys
+
+def wait_for_result():
+    try:
+        while True:
+            data = ser.readline()
+            if data:
+                #Retrun the data exactly
+                sys.stdout.write(data.decode(errors='ignore'))
+                sys.stdout.flush()
+    except Exception as e:
+        sys.stdout.write(f"\n[serial_reader stopped: {e}]\n")
 
 #Set the serial communication - baud agreed upon beforehand
 ser = serial.Serial('/dev/serial0', 115200, timeout=1)
@@ -22,4 +34,24 @@ while True:
         break
 
 #Wait for the user to print to the console
+try:
+    while(True):
+        #Read a whole line from the console - blocked until Enter
+        read_line = sys.stdin.readline()
+        #if user terminates the communication
+        if read_line == "":  # EOF
+            break
+        #Ensure we end with '!'
+        read_line += "!"
+        #Send the data and flush it
+        ser.write(read_line.encode())
+        ser.flush()
+        #Wait for the response from the D1R32
+
+except:
+    pass
+finally:
+    #Inform about closing the link and close it
+    print("\nClosing serial…")
+    ser.close()
 
